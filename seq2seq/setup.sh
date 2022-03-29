e5f97042b35b29d4f4f9f194ecdbda2181cd08ee
AzzaAbdelGhani <azza.mazher90@gmail.com>
2022-03-29 20:43:10 +0200

    modify setup file to run code on google colab

--- seq2seq/setup.sh
+++ seq2seq/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # install transformers and requirements
-pip install --upgrade -r requirements.txt
-pip install --upgrade transformers/
+pip install --upgrade -r xl-sum/seq2seq/requirements.txt
+pip install --upgrade xl-sum/seq2seq/transformers/
 
 # install rouge module and dependecies
-pip install -r ../multilingual_rouge_scoring/requirements.txt
+pip install -r xl-sum/multilingual_rouge_scoring/requirements.txt
 python -m unidic download # for japanese segmentation
-pip install --upgrade ../multilingual_rouge_scoring
+pip install --upgrade xl-sum/multilingual_rouge_scoring
 python -m nltk.downloader punkt
